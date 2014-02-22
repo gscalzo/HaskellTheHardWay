04bd6edd385992bd602a7ca3935e1475dba8480a
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-02-22 16:50:11 +0000

    fixed ex 6

--- ch03/ex06.hs
+++ ch03/ex06.hs
@@ -1,4 +1,9 @@
+import Data.Ord
 import Data.List
 
 subLSort :: [[a]] -> [[a]]
-subLSort | = subLSort (comparing length) |
+subLSort list = sortBy (comparing length) list
+
+subLSort' :: [[a]] -> [[a]]
+subLSort' list = sortBy compareLength list
+                 where compareLength a b = compare (length a) (length b)
