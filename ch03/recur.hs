af73aeb1a8cc25bcf772a8cd11a09a02676ad958
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-02-24 23:47:33 +0000

    added recursion funtions

---
+++ ch03/recur.hs
@@ -0,0 +1,12 @@
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerOrEqual = [a | a <- xs, a <= x]
+        larger = [a | a <- xs, a > x]
+    in  quicksort smallerOrEqual ++ [x] ++ quicksort larger
