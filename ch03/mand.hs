862a5086b2e3874215f8459e6b1d6e2164c66759
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-02-25 11:04:22 +0000

    moved a file

--- ch03/mand.hs
+++
@@ -1,9 +0,0 @@
-import Graphics.Rendering.OpenGL
-import Graphics.UI.GLUT
-
-main :: IO ()
-main = do 
-  (progname, _) <- getArgsAndInitialize
-  createWindow "Hello World"
-  displayCallback $= flush
-  mainLoop
