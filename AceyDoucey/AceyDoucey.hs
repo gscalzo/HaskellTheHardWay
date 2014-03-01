e5f6ea7437f5f0c28e525ad97c438ee48ef9fa61
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-01 22:11:07 +0000

    Add intro

--- AceyDoucey/AceyDoucey.hs
+++ AceyDoucey/AceyDoucey.hs
@@ -3,8 +3,21 @@
 -- The MIT License (MIT)
 -- 
 import System.Environment
+intro = ["Acey Ducey Card Game",
+	    "Adapted from a BASIC game from Creative Computing - Morristown, New Jersey.",
+	    "",
+	    "",
+	    "",
+	    "Acey Ducey is played in the following manner:",
+	    "The dealer (computer) deals two cards face up",
+	    "You have an option to bet or not to bet depending",
+	    "on whether or not you feel the card will have",
+	    "a value between the first two.",
+	    "If you do not want to bet, input a 0."]
 
 main :: IO ()
-main = getArgs >>= print . haqify . head
- 
-haqify s = "Haq! " ++ s
+main = do
+    mapM_ putStrLn intro
+
+
+
