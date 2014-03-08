0ee2b5ade56f5b8f2094d2f00cf1763aa5bb6777
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-08 23:23:32 +0000

    add double digit function

--- cis194/week1/validatingCC.hs
+++ cis194/week1/validatingCC.hs
@@ -7,3 +7,14 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev x
         | x <= 0    = []
         | otherwise = (x `mod` 10) : (toDigitsRev (x `div` 10))        
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther l = reverse (doubleEveryOther' (reverse l) )
+
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' [] = []
+doubleEveryOther' [x] = [x]
+doubleEveryOther' [x,y] = [x, (2 * y)]
+doubleEveryOther' (x:y:zs) = x : (2 * y) : (doubleEveryOther' zs)
+
