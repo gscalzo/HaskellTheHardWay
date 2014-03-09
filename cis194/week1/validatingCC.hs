2aab8980c7eb9c2d1f00e847536672db8b2d1849
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-09 21:33:52 +0000

    Add sum of digits function

--- cis194/week1/validatingCC.hs
+++ cis194/week1/validatingCC.hs
@@ -18,3 +18,7 @@ doubleEveryOther' [x] = [x]
 doubleEveryOther' [x,y] = [x, (2 * y)]
 doubleEveryOther' (x:y:zs) = x : (2 * y) : (doubleEveryOther' zs)
 
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = x `div` 10 + x `mod` 10 + (sumDigits xs)
+
