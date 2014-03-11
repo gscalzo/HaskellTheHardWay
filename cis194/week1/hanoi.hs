eb1a9bc54704b4856f21cdf7094a7592c1ac294b
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-11 11:24:23 +0000

    add basic hanoi

---
+++ cis194/week1/hanoi.hs
@@ -0,0 +1,11 @@
+-- | Main entry point to the application.
+module Main where
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 sourcePeg destPeg _ = [(sourcePeg, destPeg)]
+hanoi n sourcePeg destPeg tempPeg = hanoi (n - 1) sourcePeg tempPeg destPeg ++ [(sourcePeg, destPeg)] ++ hanoi (n - 1) tempPeg destPeg sourcePeg
+
+
