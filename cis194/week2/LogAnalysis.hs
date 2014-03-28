1554345e2bbe28c941c491a18c3b1c4989ea4528
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-28 16:07:26 +0000

    add parse message

--- cis194/week2/LogAnalysis.hs
+++ cis194/week2/LogAnalysis.hs
@@ -3,9 +3,15 @@ module LogAnalysis where
 
 import Log
 
+toInt :: String -> Int
+toInt xs = read xs
+
 parseMessage :: String -> LogMessage
 parseMessage pm = case words pm of
-                    ("I" : ts : xs)    -> LogMessage Info (read ts) (unwords xs)
-                    ("W" : ts : xs)    -> LogMessage Warning (read ts) (unwords xs)
-                    ("E" : en : ts : xs) -> LogMessage (Error (read en)) (read ts) (unwords xs)
+                    ("I" : ts : xs)    -> LogMessage Info (toInt ts) (unwords xs)
+                    ("W" : ts : xs)    -> LogMessage Warning (toInt ts) (unwords xs)
+                    ("E" : en : ts : xs) -> LogMessage (Error (toInt en)) (toInt ts) (unwords xs)
                     xs                  -> Unknown (unwords xs)
+
+parse :: String -> [LogMessage]
+parse content = map parseMessage (lines content)
