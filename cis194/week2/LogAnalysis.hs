d4be28857d8deeadd40db44642b5bcd4bc0ac632
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-28 16:02:34 +0000

    insert parseMessage

--- cis194/week2/LogAnalysis.hs
+++ cis194/week2/LogAnalysis.hs
@@ -2,3 +2,10 @@
 module LogAnalysis where
 
 import Log
+
+parseMessage :: String -> LogMessage
+parseMessage pm = case words pm of
+                    ("I" : ts : xs)    -> LogMessage Info (read ts) (unwords xs)
+                    ("W" : ts : xs)    -> LogMessage Warning (read ts) (unwords xs)
+                    ("E" : en : ts : xs) -> LogMessage (Error (read en)) (read ts) (unwords xs)
+                    xs                  -> Unknown (unwords xs)
