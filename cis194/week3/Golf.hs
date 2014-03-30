10fa22e3b67aad77f0a488e6d5acf914905c662e
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-31 00:22:09 +0100

    add LocalAmxima

--- cis194/week3/Golf.hs
+++ cis194/week3/Golf.hs
@@ -8,3 +8,18 @@ idx l = l `zip` [1..]
 
 skip :: ([a] , Int) -> [a]
 skip (l,n) = [fst x | x <- idx l, (snd x) `mod` n == 0]
+
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima l = concat . map lm $ triplets l 
+
+triplets :: [a] -> [[a]]
+triplets [] = []
+triplets [x] = []
+triplets [x,y] = []
+triplets (x:y:z:xs) = [[x,y,z]] ++ triplets (y:z:xs)
+
+lm :: [Integer] -> [Integer]
+lm [x,y,z] = if y > x && y > z
+            then [y]
+            else []
