e000e6de543bd7bbaf56d369f5684914dddc291b
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-30 23:48:45 +0100

    smaller solution

--- cis194/week3/Golf.hs
+++ cis194/week3/Golf.hs
@@ -1,15 +1,10 @@
 module Golf where
 
 skips :: [a] -> [[a]]
-skips l = map (\(x,y) -> skip (x, y)) $ fullIndexed l  
+skips l = map skip . map (\(x,y) -> (l, y)) $ idx l  
 
-indexed :: [a] -> [(a, Int)]
-indexed l = l `zip` [1..]
+idx :: [a] -> [(a, Int)]
+idx l = l `zip` [1..]
 
-fullIndexed :: [a] -> [([a], Int)]
-fullIndexed l = map (\(x,y) -> (l, y))$ indexed l 
-
--- skip ([1,2,3,4,5,6], 2) -> [2,4,6]
--- skip ([1,2,3,4,5,6], 3) -> [3,6]
 skip :: ([a] , Int) -> [a]
-skip (l,n) = [fst x | x <- l `zip` [1..], (snd x) `mod` n == 0]
+skip (l,n) = [fst x | x <- idx l, (snd x) `mod` n == 0]
