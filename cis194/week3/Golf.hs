a58564bc04a87a1a60dad65ef48c310723c4d1a2
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-03-31 17:51:42 +0100

    reduced number of chars in localmaxima

--- cis194/week3/Golf.hs
+++ cis194/week3/Golf.hs
@@ -1,12 +1,15 @@
+
 module Golf where
+
+import Data.List
 
 skips :: [a] -> [[a]]
 skips l = let idx l = l `zip` [1..] in
             let skip (l,n) = [fst x | x <- idx l, (snd x) `mod` n == 0] in
                 map skip . map (\(x,y) -> (l, y)) $ idx l  
 
-localMaxima :: [Integer] -> [Integer]
-localMaxima l = concat . map lm $ trp l 
+localMaxima' :: [Integer] -> [Integer]
+localMaxima' l = concat . map lm $ trp l 
                 where lm [x,y,z] = if y > x && y > z
                                     then [y]
                                     else []
@@ -16,3 +19,6 @@ trp [] = []
 trp [x] = []
 trp [x,y] = []
 trp (x:y:z:xs) = [[x,y,z]] ++ trp (y:z:xs)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima l = concat . map (\[x,y,z] -> if y > x && y > z then [y] else []) $ [x | x <- map (take 3) $ tails l, length x == 3]
