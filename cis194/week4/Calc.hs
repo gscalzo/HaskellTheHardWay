981111ec4a81171aa298aa21bf3b9c01a53be1b3
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-05-26 16:24:21 +0100

    ex 4

--- cis194/week4/Calc.hs
+++ cis194/week4/Calc.hs
@@ -27,3 +27,34 @@ instance Expr ExprT where
 
 reify :: ExprT -> ExprT
 reify = id
+
+instance Expr Integer where
+    lit x = x
+    add x y= x + y
+    mul x y= x * y
+
+instance Expr Bool where
+    lit x = x > 0
+    add x y= x || y
+    mul x y= x && y
+
+newtype MinMax  = MinMax Integer deriving (Eq, Show)
+newtype Mod7    = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+    lit x = MinMax x
+    add (MinMax x) (MinMax y) = MinMax (max x y)
+    mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+    lit x = Mod7 (x `mod` 7)
+    add (Mod7 x) (Mod7 y) = Mod7  ((x + y) `mod` 7)
+    mul (Mod7 x) (Mod7 y) = Mod7  ((x * y) `mod` 7)
+
+
+
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+
