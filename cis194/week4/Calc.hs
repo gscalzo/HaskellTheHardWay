b67797ee6f4d7e83ac6bdf8d7465b607acb02074
Giordano Scalzo <giordano.scalzo@gmail.com>
2014-05-26 12:52:56 +0100

    add ex2

--- cis194/week4/Calc.hs
+++ cis194/week4/Calc.hs
@@ -1,9 +1,16 @@
 module Calc where
 
 import ExprT
+import Parser
 
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add x y) = (eval x) + (eval y)
 eval (Mul x y) = (eval x) * (eval y)
+
+
+evalStr :: String -> Maybe Integer
+evalStr x = case parseExp Lit Add Mul x of
+                Nothing -> Nothing
+                Just y  -> Just (eval y)
